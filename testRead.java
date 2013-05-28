1af07912933416def4feca49a530362be73e617f
DataSciCode <srinu18@gmail.com>
2013-05-28 18:51:51 -0500

    Update testRead.java

--- testRead.java
+++ testRead.java
@@ -1,5 +1,43 @@
-public String Officer;
-  		public Date LoanDate;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Date;
+import java.lang.String;;
+ 
+
+
+public class reader {
+
+	public static void main(String[] args) {
+		
+/*		  System.out.println("2 wrd: "   );		   
+		  System.out.println("1 wrd: "   );
+		
+		   */
+		
+		//Open the file for reading
+
+		String thisLine;
+		class loanrec {
+			public String RecordType;
+			public String Action;
+			public String LoanNumber;
+			public String Name1;
+			public String Fname1;
+			public String Name2;
+			public String Fname2;
+			public String Address1;
+			public String Address2;
+			public String City;
+			public String State;
+			public String ZipCode;
+			public String Phone;
+			public String Salutation;
+			public String Branch;
+			public String Division;
+			public String Misc;
+			public String Officer;
+			public Date LoanDate;
 			public Date MaturityDate;
 			public Date PaidDate;
 			public String Balance;
@@ -92,3 +130,4 @@ public String Officer;
 }
 	
 }
+
